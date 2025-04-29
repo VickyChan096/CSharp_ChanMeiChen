28e852bef154447bd5df4ca91e555d3040eb8386
詹美珍 <103476140+VickyChan096@users.noreply.github.com>
2025-04-29 14:25:58 +0800

    Update LabOneNumberGuessing.cs

--- LabOneNumberGuessing.cs
+++ LabOneNumberGuessing.cs
@@ -19,27 +19,23 @@ namespace NumberGuessing
             //   - 沒數字可猜，失敗
 
             // Solution:
-            // 1.
-            // 建立一個 Random 物件，生成隨機數
+            // 1.建立一個 Random 物件，生成隨機數
             Random random = new Random();
             // 產生一個 0-99 的隨機整數作為秘密數字
             // random.Next(minValue, maxValue) 會生成一個大於等於 minValue 但小於 maxValue 的數字
             // 所以這裡輸入 100 會產生 0 到 99
             int secretNumber = random.Next(0, 100);
 
-            // 2.
-            // 初始化範圍
+            // 2.初始化範圍
             int lowerBound = 0;
             int upperBound = 99;
 
-            // 3-4.
             while (lowerBound <= upperBound)
             {
                 // 3.提示使用者猜測
                 Console.WriteLine($"目前猜測範圍是({lowerBound}, {upperBound})");
 
-                // 4.
-                // 初始化輸入
+                // 4.初始化輸入
                 int guess = 0;
                 bool isValidInput = false;
 
@@ -66,6 +62,7 @@ namespace NumberGuessing
 
                 }
 
+                // 5.判斷猜測結果
                 if (guess == secretNumber)
                 {
                     Console.WriteLine("bingo! 你猜對了！");
@@ -80,13 +77,13 @@ namespace NumberGuessing
                     upperBound = guess - 1;
                 }
 
-                Console.WriteLine("-----------------------------------------");
-
                 if (lowerBound == upperBound)
                 {
                     Console.WriteLine($"你輸了，答案是{secretNumber}");
                     break;
                 }
+
+                Console.WriteLine("-----------------------------------------");
             }
         }
     }
